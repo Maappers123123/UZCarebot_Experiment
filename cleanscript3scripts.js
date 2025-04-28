875f28b65d7125541324ed35ad5327c95d35b1bb
Maappers123123 <164487168+Maappers123123@users.noreply.github.com>
2025-04-28 10:47:36 +0200

    Update cleanscript3scripts.js

--- cleanscript3scripts.js
+++ cleanscript3scripts.js
@@ -1,18 +1,4 @@
-    const messenger = document.querySelector('df-messenger');
-
-messenger.addEventListener('df-chip-clicked', function(event) {
-  const clickedText = event.detail.text;
-
-  if (clickedText.includes('spelletje')) {
-    showGame();
-  } else if (clickedText.includes('video')) {
-    showVideoScreen();
-  } else if (clickedText.includes('Ontspan')) {
-    showMindfulness();
-  }
-});
-
-function toggleMenu() {
+    function toggleMenu() {
       const menu = document.getElementById('dropdown-menu');
       menu.classList.toggle('hidden');
     }
